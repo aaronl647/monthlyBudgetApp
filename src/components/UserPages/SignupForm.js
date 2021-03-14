42f78a8e7a7fd788eaf7da57d2dd3a1acde13119
Aaron Lei <aaron.l@live.ca>
2021-03-14 15:55:04 -0400

    signup form component created

--- src/components/UserPages/SignupForm.js
+++ src/components/UserPages/SignupForm.js
@@ -1,11 +1,85 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-function SignupForm() {
-  return (
-    <div>
-      <h3>Sign up form here!</h3>
-    </div>
-  );
+export default class SignupForm extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    passwordConf: "",
+  };
+  handleChange = (e) => {
+    this.props.updateMessage("");
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log("await goes here");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  isFormValid() {
+    return !(
+      this.state.name &&
+      this.state.email &&
+      this.state.password === this.state.passwordConf
+    );
+  }
+  render() {
+    return (
+      <div>
+        <header className="header-footer">Sign Up</header>
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                value={this.state.name}
+                name="name"
+                onChange={this.handleChange}
+              />
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                value={this.state.email}
+                name="email"
+                onChange={this.handleChange}
+              />
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                value={this.state.password}
+                name="password"
+                onChange={this.handleChange}
+              />
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password Confirmation"
+                value={this.state.passwordConf}
+                name="passwordConf"
+                onChange={this.handleChange}
+              />
+              <input
+                name="submit"
+                type="button"
+                className="btn btn-primary"
+                value="Submit"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default SignupForm;
