b8b06f8e7a6ff7e9dce458b67caa0303320d8db1
Aaron Lei <aaron.l@live.ca>
2021-04-04 12:15:50 -0400

    created SummarySection component

--- src/components/Breakdown/Breakdown.js
+++ src/components/Breakdown/Breakdown.js
@@ -1,31 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Breakdown.css";
 import budgetService from "../../utils/budgetService";
-
-function Breakdown() {
-  return (
-    <div>
-      <h1>Breakdown</h1>
-      <h2>General Breakdown</h2>
-      <div className="summary-section">
-        <h4>Income</h4>
-        <h4>Expense</h4>
-        <h4>Remainder</h4>
+import SummarySection from "../SummarySection/SummarySection";
+export default class Breakdown extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Breakdown</h1>
+        <h2>General Breakdown</h2>
+        <div className="summary-section">
+          <SummarySection />
+        </div>
+        <h2>Detailed Breakdown</h2>
+        <div className="detailed-breakdown">
+          <h4>Guilt-Free Spending</h4>
+          <h4>Savings</h4>
+          <h4>Investments</h4>
+        </div>
+        <div className="chart-container">
+          <h2>Charts </h2>
+        </div>
+        <div className="upcoming-container">
+          <h2>Upcoming Payments </h2>
+        </div>
       </div>
-      <h2>Detailed Breakdown</h2>
-      <div className="detailed-breakdown">
-        <h4>Guilt-Free Spending</h4>
-        <h4>Savings</h4>
-        <h4>Investments</h4>
-      </div>
-      <div className="chart-container">
-        <h2>Charts </h2>
-      </div>
-      <div className="upcoming-container">
-        <h2>Upcoming Payments </h2>
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Breakdown;
---
+++ src/components/SummarySection/SummarySection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SummarySection() {
+  return (
+    <div>
+      <h4>Income</h4>
+      <h4>Expense</h4>
+      <h4>Remainder</h4>
+    </div>
+  );
+}
+
+export default SummarySection;
