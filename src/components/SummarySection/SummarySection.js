90853585919e79cf4e4611c9509795f94a151d82
Aaron Lei <aaron.l@live.ca>
2021-04-05 18:04:03 -0400

    added functions to summarySection component

--- src/components/SummarySection/SummarySection.js
+++ src/components/SummarySection/SummarySection.js
@@ -2,46 +2,61 @@ import React, { Component } from "react";
 import budgetService from "../../utils/budgetService";
 
 export default class SummarySection extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
     this.state = {
-      totalIncome: "",
-      totalExpense: "",
+      income: [],
+      expense: [],
     };
+  }
+  componentDidMount() {
+    budgetService.income().then((income) => {
+      let result = income.map((a) => a.amount);
+      this.setState({ income: result });
+    });
+
+    budgetService.expense().then((expense) => {
+      let result = expense.map((a) => a.amountDue);
+      this.setState({ expense: result });
+    });
+  }
+
+  incomeTotal() {
+    const incTotal = this.state.income.reduce(
+      (totalInc, income) => totalInc + income,
+      0
+    );
+    return incTotal;
+  }
+
+  expenseTotal() {
+    const expTotal = this.state.expense.reduce(
+      (totalExp, expense) => totalExp + expense,
+      0
+    );
+    return expTotal;
+  }
+
+  remainder() {
+    let difference = this.incomeTotal() - this.expenseTotal();
+    return difference;
   }
 
   render() {
     return (
       <div>
-        <h4>Income</h4>
         <div>
-          <h5>{}</h5>
+          <h4>Income</h4>
+          <h5>$ {this.incomeTotal()}</h5>
         </div>
-        <h4>Expense</h4>
         <div>
-          <h5>{}</h5>
+          <h4>Expense</h4>
+          <h5>$ {this.expenseTotal()}</h5>
         </div>
         <h4>Remainder</h4>
+        <h5>$ {this.remainder()}</h5>
       </div>
     );
   }
 }
-
-//  var totalInc = () => {
-//    budgetService.income().then((income) => {
-//      let result = income.map((a) => a.amount);
-//      const reducer = (acc, currentValue) => acc + currentValue;
-//      let total = result.reduce(reducer);
-//      console.log(total);
-//      return <p>{total}</p>;
-//    });
-//  };
-
-//  var totalExp = () => {
-//    budgetService.expense().then((expense) => {
-//      let result = expense.map((a) => a.amountDue);
-//      const reducer = (acc, currentValue) => acc + currentValue;
-//      let total = result.reduce(reducer);
-//      console.log(total);
-//    });
-//  };
